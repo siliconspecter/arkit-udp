4dcb8f1efede6b81b804695827adc1e5bb741d54
siliconspecter <siliconspecter@outlook.com>
2025-06-15 22:29:15 +0100

    Initial commit.

---
+++ arkit-udp/arkit_udpApp.swift
@@ -0,0 +1,17 @@
+//
+//  arkit_udpApp.swift
+//  arkit-udp
+//
+//  Created by siliconspecter on 15/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct arkit_udpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
