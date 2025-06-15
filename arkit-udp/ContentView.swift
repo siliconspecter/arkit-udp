7b507692c5293c16d061b3dedfdf76d9fbbb97f4
siliconspecter <siliconspecter@outlook.com>
2025-06-15 23:59:33 +0100

    Settings now auto-save.

--- arkit-udp/ContentView.swift
+++ arkit-udp/ContentView.swift
@@ -8,14 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var faceTrackingEnabled: Bool = false
-    @State var udpConnectionEnabled: Bool = false
+    @SceneStorage("ContentView.faceTrackingEnabled") private var faceTrackingEnabled = false
+    @SceneStorage("ContentView.udpConnectionEnabled") private var udpConnectionEnabled = false
     
-    @State var ipA: UInt8?
-    @State var ipB: UInt8?
-    @State var ipC: UInt8?
-    @State var ipD: UInt8?
-    @State var port: UInt16?
+    @SceneStorage("ContentView.ipA") private var ipAInt: Int?
+    var ipA: Binding<UInt8?> {
+        Binding(
+            get: { ipAInt.flatMap { UInt8(exactly: $0) } },
+            set: { ipAInt = $0.map { Int($0) } }
+        )
+    }
+    
+    @SceneStorage("ContentView.ipB") private var ipBInt: Int?
+    var ipB: Binding<UInt8?> {
+        Binding(
+            get: { ipBInt.flatMap { UInt8(exactly: $0) } },
+            set: { ipBInt = $0.map { Int($0) } }
+        )
+    }
+    
+    @SceneStorage("ContentView.ipC") private var ipCInt: Int?
+    var ipC: Binding<UInt8?> {
+        Binding(
+            get: { ipCInt.flatMap { UInt8(exactly: $0) } },
+            set: { ipCInt = $0.map { Int($0) } }
+        )
+    }
+    
+    @SceneStorage("ContentView.ipD") private var ipDInt: Int?
+    var ipD: Binding<UInt8?> {
+        Binding(
+            get: { ipDInt.flatMap { UInt8(exactly: $0) } },
+            set: { ipDInt = $0.map { Int($0) } }
+        )
+    }
+    
+    @SceneStorage("ContentView.port") private var portInt: Int?
+    var port: Binding<UInt16?> {
+        Binding(
+            get: { portInt.flatMap { UInt16(exactly: $0) } },
+            set: { portInt = $0.map { Int($0) } }
+        )
+    }
     
     var body: some View {
         NavigationView {
@@ -30,29 +64,29 @@ struct ContentView: View {
                     Toggle(isOn: $udpConnectionEnabled) {
                             Text("Enabled")
                     }
-                    .disabled(ipA == nil || ipB == nil || ipC == nil || ipD == nil || port == nil)
+                    .disabled(ipAInt == nil || ipBInt == nil || ipCInt == nil || ipDInt == nil || portInt == nil)
                     
                     LabeledContent("IP Address") {
                         HStack {
-                            TextField("192", value: $ipA, format: .number)
+                            TextField("192", value: ipA, format: .number)
                                 .disabled(udpConnectionEnabled)
                                 .frame(width: 40)
                                 .fixedSize(horizontal: true, vertical: false)
                                 .keyboardType(.numberPad)
                             
-                            TextField("168", value: $ipB, format: .number)
+                            TextField("168", value: ipB, format: .number)
                                 .disabled(udpConnectionEnabled)
                                 .frame(width: 40)
                                 .fixedSize(horizontal: true, vertical: false)
                                 .keyboardType(.numberPad)
                             
-                            TextField("1", value: $ipC, format: .number)
+                            TextField("1", value: ipC, format: .number)
                                 .disabled(udpConnectionEnabled)
                                 .frame(width: 40)
                                 .fixedSize(horizontal: true, vertical: false)
                                 .keyboardType(.numberPad)
                             
-                            TextField("1", value: $ipD, format: .number)
+                            TextField("1", value: ipD, format: .number)
                                 .disabled(udpConnectionEnabled)
                                 .frame(width: 40)
                                 .fixedSize(horizontal: true, vertical: false)
@@ -61,7 +95,7 @@ struct ContentView: View {
                     }
                     
                     LabeledContent("Port") {
-                        TextField("6772", value: $port, format: .number)
+                        TextField("6772", value: port, format: .number)
                             .disabled(udpConnectionEnabled)
                             .keyboardType(.numberPad)
                     }
