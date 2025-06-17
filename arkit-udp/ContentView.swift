50dfb14ea028226dac0ca112430aaa04f1d1e7f6
siliconspecter <siliconspecter@outlook.com>
2025-06-17 20:29:11 +0100

    Move into integrations object.

--- arkit-udp/ContentView.swift
+++ arkit-udp/ContentView.swift
@@ -7,87 +7,111 @@
 
 import SwiftUI
 
+class Integrations : ObservableObject {    
+    @AppStorage("ContentView.faceTrackingEnabled") private var faceTrackingEnabledBool = false
+    
+    var faceTrackingEnabled: Bool {
+        get { self.faceTrackingEnabledBool }
+        set {
+            self.faceTrackingEnabledBool = newValue
+        }
+    }
+    
+    @AppStorage("ContentView.udpConnectionEnabled") private var udpConnectionEnabledBool = false
+    
+    var udpConnectionEnabled: Bool {
+        get { self.udpConnectionEnabledBool }
+        set {
+            self.udpConnectionEnabledBool = newValue
+        }
+    }
+    
+    @AppStorage("ContentView.ipA") private var ipAInt: Int?
+    
+    var ipA: UInt8? {
+        get { self.ipAInt.flatMap { UInt8(exactly: $0) } }
+        set {
+            self.ipAInt = newValue.map { Int($0) }
+        }
+    }
+    
+    @AppStorage("ContentView.ipB") private var ipBInt: Int?
+
+    var ipB: UInt8? {
+        get { self.ipBInt.flatMap { UInt8(exactly: $0) } }
+        set {
+            self.ipBInt = newValue.map { Int($0) }
+        }
+    }
+    
+    @AppStorage("ContentView.ipC") private var ipCInt: Int?
+    
+    var ipC: UInt8? {
+        get { self.ipCInt.flatMap { UInt8(exactly: $0) } }
+        set {
+            self.ipCInt = newValue.map { Int($0) }
+        }
+    }
+    
+    @AppStorage("ContentView.ipD") private var ipDInt: Int?
+    
+    var ipD: UInt8? {
+        get { self.ipDInt.flatMap { UInt8(exactly: $0) } }
+        set {
+            self.ipDInt = newValue.map { Int($0) }
+        }
+    }
+    
+    @AppStorage("ContentView.port") private var portInt: Int?
+    
+    var port: UInt16? {
+        get { self.portInt.flatMap { UInt16(exactly: $0) } }
+        set {
+            self.portInt = newValue.map { Int($0) }
+        }
+    }
+}
+
 struct ContentView: View {
-    @SceneStorage("ContentView.faceTrackingEnabled") private var faceTrackingEnabled = false
-    @SceneStorage("ContentView.udpConnectionEnabled") private var udpConnectionEnabled = false
-    
-    @SceneStorage("ContentView.ipA") private var ipAInt: Int?
-    var ipA: Binding<UInt8?> {
-        Binding(
-            get: { ipAInt.flatMap { UInt8(exactly: $0) } },
-            set: { ipAInt = $0.map { Int($0) } }
-        )
-    }
-    
-    @SceneStorage("ContentView.ipB") private var ipBInt: Int?
-    var ipB: Binding<UInt8?> {
-        Binding(
-            get: { ipBInt.flatMap { UInt8(exactly: $0) } },
-            set: { ipBInt = $0.map { Int($0) } }
-        )
-    }
-    
-    @SceneStorage("ContentView.ipC") private var ipCInt: Int?
-    var ipC: Binding<UInt8?> {
-        Binding(
-            get: { ipCInt.flatMap { UInt8(exactly: $0) } },
-            set: { ipCInt = $0.map { Int($0) } }
-        )
-    }
-    
-    @SceneStorage("ContentView.ipD") private var ipDInt: Int?
-    var ipD: Binding<UInt8?> {
-        Binding(
-            get: { ipDInt.flatMap { UInt8(exactly: $0) } },
-            set: { ipDInt = $0.map { Int($0) } }
-        )
-    }
-    
-    @SceneStorage("ContentView.port") private var portInt: Int?
-    var port: Binding<UInt16?> {
-        Binding(
-            get: { portInt.flatMap { UInt16(exactly: $0) } },
-            set: { portInt = $0.map { Int($0) } }
-        )
-    }
+    @ObservedObject var integrations = Integrations()
     
     var body: some View {
         NavigationView {
             Form {
                 Section(header: Text("Face Tracking"), content: {
-                    Toggle(isOn: $faceTrackingEnabled) {
+                    Toggle(isOn: $integrations.faceTrackingEnabled) {
                             Text("Enabled")
                     }
                 })
                 
                 Section(header: Text("UDP Connection"), content: {
-                    Toggle(isOn: $udpConnectionEnabled) {
+                    Toggle(isOn: $integrations.udpConnectionEnabled) {
                             Text("Enabled")
                     }
-                    .disabled(ipAInt == nil || ipBInt == nil || ipCInt == nil || ipDInt == nil || portInt == nil)
+                    .disabled(integrations.ipA == nil || integrations.ipB == nil || integrations.ipC == nil || integrations.ipD == nil || integrations.port == nil)
                     
                     LabeledContent("IP Address") {
                         HStack {
-                            TextField("192", value: ipA, format: .number)
-                                .disabled(udpConnectionEnabled)
+                            TextField("192", value: $integrations.ipA, format: .number)
+                                .disabled(integrations.udpConnectionEnabled)
                                 .frame(width: 40)
                                 .fixedSize(horizontal: true, vertical: false)
                                 .keyboardType(.numberPad)
                             
-                            TextField("168", value: ipB, format: .number)
-                                .disabled(udpConnectionEnabled)
+                            TextField("168", value: $integrations.ipB, format: .number)
+                                .disabled(integrations.udpConnectionEnabled)
                                 .frame(width: 40)
                                 .fixedSize(horizontal: true, vertical: false)
                                 .keyboardType(.numberPad)
                             
-                            TextField("1", value: ipC, format: .number)
-                                .disabled(udpConnectionEnabled)
+                            TextField("1", value: $integrations.ipC, format: .number)
+                                .disabled(integrations.udpConnectionEnabled)
                                 .frame(width: 40)
                                 .fixedSize(horizontal: true, vertical: false)
                                 .keyboardType(.numberPad)
                             
-                            TextField("1", value: ipD, format: .number)
-                                .disabled(udpConnectionEnabled)
+                            TextField("1", value: $integrations.ipD, format: .number)
+                                .disabled(integrations.udpConnectionEnabled)
                                 .frame(width: 40)
                                 .fixedSize(horizontal: true, vertical: false)
                                 .keyboardType(.numberPad)
@@ -95,8 +119,8 @@ struct ContentView: View {
                     }
                     
                     LabeledContent("Port") {
-                        TextField("6772", value: port, format: .number)
-                            .disabled(udpConnectionEnabled)
+                        TextField("6772", value: $integrations.port, format: .number)
+                            .disabled(integrations.udpConnectionEnabled)
                             .keyboardType(.numberPad)
                     }
                 })
